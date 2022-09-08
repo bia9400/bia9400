7e0e7a04dadd09a6d4f0382d7052eeec5875818e
bia9400 <ferro.biagio@gmail.com>
2022-09-08 14:12:36 +0200

    wefqwf

--- try.js
+++ try.js
@@ -1,8 +1,21 @@
+
+
+
+
+
 const Full_Stack_Web_Developer={
+
     name: "Biagio Ferro",
-    message: "Ciao, Benvenuto sul mio profilo. Sotto potrai trovare alcuni dei progetti su cui ho lavorato."
+    message: "Ciao, Benvenuto sul mio profilo. Sotto potrai trovare alcuni dei progetti su cui ho lavorato.",
+    skills:["Laravel","Vue","SQL","PHP","SASS","Javascript","HTML5","CSS"]
 
 }
+
+
+
+
+
+
 
 
 
