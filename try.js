82d133aaa42337ea4181fd2880f193e0fc6d7d96
bia9400 <101462973+bia9400@users.noreply.github.com>
2022-09-09 15:16:54 +0200

    Update try.js

--- try.js
+++ try.js
@@ -6,8 +6,16 @@
 const Full_Stack_Web_Developer={
 
     name: "Biagio Ferro",
-    message: "Ciao, Benvenuto sul mio profilo. Sotto potrai trovare alcuni dei progetti su cui ho lavorato.",
-    skills:["Laravel","Vue","SQL","PHP","SASS","Javascript","HTML5","CSS"]
+
+    message: `Benvenuto sul mio profilo, sono uno Junior Full-Stack Web Developer.
+              Ho una spiccata curiosità per tutto ciò che mi circonda, dalla tecnologia
+              alla musica e dai processi di panificazione a quelli brassicoli. 
+              Da sempre ho progettato, modificato e studiato tutto ciò che mi appassionava,
+              ottenendo senza rendermene conto delle buone capacità di problem solving.`,
+
+    skills:["Laravel","Vue","SQL","PHP","SASS","Javascript","HTML5","CSS","Bootstrap"],
+
+    hobbies: ["Adoro l'informatica","Sono un Musicista","Faccio ciclismo ed immersioni","Produco Birra"]
 
 }
 
